a5188e7f0fc742dd77a246fff2912049cc05c325
Pawluh <pawluh.p@gmail.com>
2020-05-25 19:49:01 +0200

    Folders
    Download folders from account

--- src/com/barosanu/controller/LoginWindowController.java
+++ src/com/barosanu/controller/LoginWindowController.java
@@ -5,12 +5,16 @@ import com.barosanu.controller.services.LoginService;
 import com.barosanu.model.EmailAccount;
 import com.barosanu.view.ViewFactory;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class LoginWindowController extends BaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginWindowController extends BaseController implements Initializable {
 
     @FXML
     private TextField emailAddressField;
@@ -31,17 +35,32 @@ public class LoginWindowController extends BaseController {
         if(fieldsAreValid()){
             EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
             LoginService loginService = new LoginService(emailAccount, emailmanager);
-            EmailLoginResult emailLoginResult = loginService.login();
+            loginService.start();
+            loginService.setOnSucceeded(event ->{
+                EmailLoginResult emailLoginResult = loginService.getValue();
+                switch (emailLoginResult){
+                    case SUCCESS:
+                        System.out.println("Login successful" + emailAccount);
+                        if(!viewFactory.isMainViewInitialized()){ // pokaz jedno okno przy dodawaniu nowego konta
+                            viewFactory.showMainWindow();
+                        }
+                        Stage stage = (Stage) errorLabel.getScene().getWindow();
+                        viewFactory.closeStage(stage);
+                        return;
+                    case FAILED_BY_CREDENTIALS:
+                        errorLabel.setText("invalid credentials");
+                        return;
+                    case FAILED_BY_UNEXPECTED_ERROR:
+                        errorLabel.setText("Unexpected Error");
+                        return;
+                    default:
+                        return;
+                }
+            });
 
-            switch (emailLoginResult){
-                case SUCCESS:
-                    System.out.println("Login successful" + emailAccount);
-            }
         }
 
-//        viewFactory.showMainWindow();
-//        Stage stage = (Stage) errorLabel.getScene().getWindow();
-//        viewFactory.closeStage(stage);
+
     }
 
     private boolean fieldsAreValid() {
@@ -57,4 +76,9 @@ public class LoginWindowController extends BaseController {
         return true;
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        emailAddressField.setText("");
+        passwordField.setText("");
+    }
 }
--- src/com/barosanu/view/ViewFactory.java
+++ src/com/barosanu/view/ViewFactory.java
@@ -17,10 +17,15 @@ public class ViewFactory {
 
     private EmailManager emailManager;
     private ArrayList<Stage> activeStages;
+    private boolean mainViewInitialized = false;
 
     public ViewFactory(EmailManager emailManager) {
         this.emailManager = emailManager;
         activeStages = new ArrayList<Stage>();
+    }
+
+    public boolean isMainViewInitialized(){
+        return mainViewInitialized;
     }
 
     // view options handling:
@@ -52,8 +57,10 @@ public class ViewFactory {
 
     public void showMainWindow(){
         System.out.println("show mainWindow");
+
         BaseController controller = new MainWindowController(emailManager, this, "MainWindow.fxml");
         initializeStage(controller);
+        mainViewInitialized = true;
     }
 
     public void showOptionsWindow(){
