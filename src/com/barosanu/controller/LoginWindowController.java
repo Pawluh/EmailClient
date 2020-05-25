f769265f7861f3f9df54d63bca48812e346daaeb
Pawluh <pawluh.p@gmail.com>
2020-05-25 19:45:47 +0200

    2

--- src/com/barosanu/controller/LoginWindowController.java
+++ src/com/barosanu/controller/LoginWindowController.java
@@ -1,6 +1,8 @@
 package com.barosanu.controller;
 
 import com.barosanu.EmailManager;
+import com.barosanu.controller.services.LoginService;
+import com.barosanu.model.EmailAccount;
 import com.barosanu.view.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -25,10 +27,34 @@ public class LoginWindowController extends BaseController {
 
     @FXML
     void loginButtonAction() {
-        System.out.println("Login Button Clcik!");
-        viewFactory.showMainWindow();
-        Stage stage = (Stage) errorLabel.getScene().getWindow();
-        viewFactory.closeStage(stage);
+        System.out.println("Login Button Click!");
+        if(fieldsAreValid()){
+            EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
+            LoginService loginService = new LoginService(emailAccount, emailmanager);
+            EmailLoginResult emailLoginResult = loginService.login();
+
+            switch (emailLoginResult){
+                case SUCCESS:
+                    System.out.println("Login successful" + emailAccount);
+            }
+        }
+
+//        viewFactory.showMainWindow();
+//        Stage stage = (Stage) errorLabel.getScene().getWindow();
+//        viewFactory.closeStage(stage);
+    }
+
+    private boolean fieldsAreValid() {
+        if(emailAddressField.getText().isEmpty()){
+            errorLabel.setText("Proszę wypełnij pole");
+            return false;
+        }
+        if(passwordField.getText().isEmpty()){
+            errorLabel.setText("Proszę wypełnij pole");
+            return false;
+        }
+
+        return true;
     }
 
 }
